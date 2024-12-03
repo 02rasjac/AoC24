8f1d537c1d637e93c0562c8741c18acca736e843
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2024-12-03 15:15:06 +0100

    Day 3.1 done

---
+++ Day3/Program.cs
@@ -0,0 +1,23 @@
+﻿// #define IS_SAMPLE
+
+using System.Text.RegularExpressions;
+
+#if IS_SAMPLE
+const string path = "sample.txt";
+#else
+const string path = "data.txt";
+#endif
+
+var data = File.ReadAllText(path);
+
+var res = 0;
+
+var match = Regex.Match(data, @"mul\(\d{1,3},\d{1,3}\)");
+while (match.Success)
+{
+    var numbers = Regex.Matches(match.Value, @"(\d{1,3})");
+    res += int.Parse(numbers[0].Value) * int.Parse(numbers[1].Value);
+    match = match.NextMatch();
+}
+
+Console.WriteLine(res);
