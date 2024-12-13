2a55ef10aeb21452e843c0a32aeddaeda0f36f1b
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2024-12-13 20:34:02 +0100

    Finish comment/explanation

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -24,98 +24,6 @@ foreach (string stone in stones)
 var blink = 0;
 const int maxBlinks = 75;
 
-// Every blink, perform the main algorithm on each stone and add the amount of times each new engraving comes in the next blink
-// as a dictionary (Example below)
-// We then only have to perform the main algo once for each number, but store add the number in the value
-/* 0
- * {
- * 0: 1
- * }
- */
-/* 1
- * {
- * 0: 0
- * 1: 1
- * }
- */
-/* 2024
- * {
- * 0: 0
- * 1: 0
- * 2024: 1
- * }
- */
-/* 20 24
- * {
- * 0: 0
- * 1: 0
- * 20: 1
- * 24: 1
- * 2024: 0
- * }
- */
-/* 2 0 2 4
- * {
- * 0: 1
- * 1: 0
- * 2: 2
- * 4: 1
- * 20: 0
- * 24: 0
- * 2024: 0
- * }
- */
-/* 4048 1 4048 8096
- * {
- * 0: 0
- * 1: 1
- * 2: 0
- * 4: 0
- * 20: 0
- * 24: 0
- * 2024: 0
- * 4048: 2
- * 8096: 1
- * }
- */
-/* 40 48 2024 40 48 80 96
- * {
- * 0: 0
- * 1: 0
- * 2: 0
- * 4: 0
- * 20: 0
- * 24: 0
- * 40: 2
- * 48: 2
- * 80: 1
- * 96: 1
- * 2024: 1
- * 4048: 0
- * 8096: 0
- * }
- */
-/* 4 0 4 8 20 24 4 0 4 8 8 0 9 6
- * {
- * 0: 3
- * 1: 0
- * 2: 0
- * 4: 4
- * 6: 3
- * 8: 3
- * 9: 1
- * 20: 1
- * 24: 1
- * 40: 0
- * 48: 0
- * 80: 0
- * 96: 0
- * 2024: 0
- * 4048: 0
- * 8096: 0
- * }
- */
-
 while (blink < maxBlinks)
 {
     if (useCount1)
@@ -147,6 +55,7 @@ Console.WriteLine($"Part 1: {nStones}");
 Console.WriteLine($"Elapsed time: {Stopwatch.GetElapsedTime(startTime)}");
 // Part 1: 186175
 // Part 2: 220566831337810
+return;
 
 void CheckStone(string stone, long multiplier, Dictionary<string, long> stonesCount)
 {
@@ -175,3 +84,64 @@ void AddOrIncrement(string stone, long multiplier, Dictionary<string, long> ston
     if (!stonesCount.TryAdd(stone, multiplier))
         stonesCount[stone] += multiplier;
 }
+
+// Every blink, perform the main algorithm on each stone and store the amount of times each new engraving comes in the next blink
+// as a dictionary (Example below)
+// We then only have to perform the main algo once for each number, and use the value from the dict as a multiplier when
+// adding how many of the new number is created
+// When it's done, sum the number each stone exists to get the total number of stones
+/* 0
+ * 1{
+ * 0: 1
+ * }
+ */
+/* 1
+ * 2{
+ * 1: 1
+ * }
+ */
+/* 2024
+ * 1{
+ * 2024: 1
+ * }
+ */
+/* 20 24
+ * 2{
+ * 20: 1
+ * 24: 1
+ * }
+ */
+/* 2 0 2 4
+ * 1{
+ * 0: 1
+ * 2: 2
+ * 4: 1
+ * }
+ */
+/* 4048 1 4048 8096
+ * 2{
+ * 1: 1
+ * 4048: 2
+ * 8096: 1
+ * }
+ */
+/* 40 48 2024 40 48 80 96
+ * 1{
+ * 40: 2
+ * 48: 2
+ * 80: 1
+ * 96: 1
+ * 2024: 1
+ * }
+ */
+/* 4 0 4 8 20 24 4 0 4 8 8 0 9 6
+ * 2{
+ * 0: 3
+ * 4: 4
+ * 6: 3
+ * 8: 3
+ * 9: 1
+ * 20: 1
+ * 24: 1
+ * }
+ */
